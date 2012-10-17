5385cd83b2a61c3f522d8de820ad771764ed4fa5
libra guest <mail@johanneshund.de>
2012-10-17 17:10:49 -0400

    routes and controller working, update does not succeed

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var mongoose = require('mongoose');
 console.log('Got modules...');
 
 //var db = mongoose.createConnection('localhost', 'ntrain');
+//yes, you can read the pw. crazy days we're living in...
 var db = mongoose.createConnection('mongodb://ntrain:ntrain@alex.mongohq.com:10011/h0ru5');
 console.log('Got db...');
 
@@ -17,6 +18,8 @@ console.log('Loaded Schemas...');
 var User = db.model("User",UserSchema);
 var Event = db.model("Event",EventSchema);
 console.log('Created Models...');
+
+//TBD: upstream this in a fork of express-mongoose-resource
 
 //var ModelCtrl = require('./controllers/modelctrl.js');
 //var eventCtrl = ModelCtrl.control(Event);
@@ -31,7 +34,10 @@ console.log('Created Controllers...');
 
 
 var r_user = app.resource(userCtrl.name,userCtrl.getExpressResourceActions(),{model: User});
-var r_user = app.resource(eventCtrl.name,eventCtrl.getExpressResourceActions(),{model: Event});
+userCtrl.resource = r_user;
+
+var r_event = app.resource(eventCtrl.name,eventCtrl.getExpressResourceActions(),{model: Event});
+eventCtrl.resource = r_event;
 //var r_event = app.resource("events",eventCtrl,{model: Event});
 console.log('Created Resources...');
 
